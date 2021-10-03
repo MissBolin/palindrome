643e112929f166f60e48d9ae724b2af4b91aa898
missbolin <samanthabolin1@gmail.com>
2021-10-02 19:46:52 -0700

    version 1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    phrase: "tacocat",
+    message: null
+  } );
 });
+
+/*POST the form data*/
+router.post('/', function(req, res){
+  res.render('index', {
+    phrase: req.body.userText,
+    message: getResultDescription(req.body.userText)
+  })
+})
+
+function checkPalindrome(phrase){
+  let temp = phrase.split("");
+  temp = temp.reverse();
+  temp = temp.join("")
+  if (phrase.toLowerCase().replace(" ", "") === temp.toLowerCase().replace(" ", "") ){
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+function getResultDescription (phrase){
+  if (checkPalindrome(phrase)) {
+    return `${phrase} is a palindrome.`
+  }
+  return `${phrase} is not a palindrome.`
+}
+
 
 module.exports = router;
